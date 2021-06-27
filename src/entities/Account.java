5c81f5101cd8de48b6597b6413c9b27677533acb
Felipe Oliveira <flpbrandao@hotmail.com>
2021-06-27 09:19:42 -0300

    Operações na expense e registros feitos em txt

---
+++ src/entities/Account.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Account {
+
+	private Double accountBalance;
+	private String accountName;
+
+	public Account() {
+
+	}
+
+	public Double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(Double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+}
